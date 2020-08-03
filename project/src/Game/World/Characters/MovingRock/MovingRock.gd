51d4e4a3ca30eebee81feb249123e8f1b32c809d
Piotr 'dyedMagenta' Frankiewicz <dyedmagenta@gmail.com>
2020-08-03 13:05:28 +0200

    Fix moving rock issues, add moving rock focused level, add moving rocks to other levels

--- project/src/Game/World/Characters/MovingRock/MovingRock.gd
+++ project/src/Game/World/Characters/MovingRock/MovingRock.gd
@@ -1,19 +1,11 @@
 extends Character
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# FIXME: Blocks some tiles it was in :v
 func try_to_move(direction : Vector2):
 	var current_map_pos = tile_map.world_to_map(position)
 	var target_pos = current_map_pos + direction
-	var world_target_pos = tile_map.map_to_world(target_pos)
-	move_to(world_target_pos)
+	var target_object = tile_map.get_world_object_from_map_pos(target_pos)
+	var current_anim = animation_player.current_animation
 	
+	if target_object == null && current_anim == "":
+		var world_target_pos = tile_map.map_to_world(target_pos)
+		move_to(world_target_pos)
