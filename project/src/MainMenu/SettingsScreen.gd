e53f9a9ff72b00688963e4dada5e245dcb37ac64
Ajver <zawlockidominik@gmail.com>
2020-09-11 02:50:16 +0200

    Saving and loading setting to file

--- project/src/MainMenu/SettingsScreen.gd
+++ project/src/MainMenu/SettingsScreen.gd
@@ -4,5 +4,6 @@ onready var master_volume_slider : HSlider = find_node("MasterVolumeSlider")
 
 
 func _ready():
-	master_volume_slider.connect("value_changed", Settings, "set_master_volume_range")
+	master_volume_slider.connect("value_changed", Settings, "set_master_volume")
 	Settings.connect("master_volume_changed", master_volume_slider, "set_value")
+	master_volume_slider.value = Settings.master_volume
--- project/src/PreloadedScripts/Settings.gd
+++ project/src/PreloadedScripts/Settings.gd
@@ -3,25 +3,45 @@ extends Node
 signal muted_changed(muted)
 signal master_volume_changed(master_volume)
 
+const SETTINGS_FILE_PATH := "user://settings.json"
+
 const MAX_VOLUME := 0.0
 const MIN_VOLUME := -70.0
 
 # Value between 0 - 1
-var master_volume := 0.5 setget set_master_volume_range
+var master_volume := 0.8 setget set_master_volume
 var muted := false setget set_muted
 
 
 func _ready() -> void:
-	_update_master_bus_volume()
-	call_deferred("_load_settings")
+	_load_from_file()
 
 
-func _load_settings() -> void:
-	set_master_volume_range(master_volume, true)
+func _load_from_file() -> void:
+	var file := File.new()
+	var needs_save := true
+	
+	if file.file_exists(SETTINGS_FILE_PATH):
+		file.open(SETTINGS_FILE_PATH, File.READ)
+		var str_file_content := file.get_as_text()
+		
+		if str_file_content:
+			var settings_dict = JSON.parse(str_file_content).result
+			
+			if settings_dict:
+				if settings_dict.has("master_volume"):
+					master_volume = settings_dict["master_volume"]
+				
+				needs_save = false
+	
+	set_master_volume(master_volume, true, false)
+	
+	if needs_save:
+		_save_to_file()
 
 
 # Takes value between 0 and 1 (0 means min volume, 1 mens max volume)
-func set_master_volume_range(val:float, force_notification:bool=false) -> void:
+func set_master_volume(val:float, override_muted:bool=true, save:bool=true) -> void:
 	var previous_master_volume := master_volume
 	master_volume = val
 	
@@ -30,17 +50,21 @@ func set_master_volume_range(val:float, force_notification:bool=false) -> void:
 	
 	_update_master_bus_volume()
 	
-	if previous_master_volume != master_volume or force_notification:
+	if previous_master_volume != master_volume:
 		emit_signal("master_volume_changed", master_volume)
 	
-	var was_muted := muted
-	muted = master_volume <= 0.0
+	if override_muted:
+		var was_muted := muted
+		muted = master_volume <= 0.0
+		
+		if was_muted != muted:
+			emit_signal("muted_changed", muted)
 	
-	if was_muted != muted or force_notification:
-		emit_signal("muted_changed", muted)
+	if save:
+		_save_to_file()
 
 
-func set_muted(m:bool, force_notification:bool=false) -> void:
+func set_muted(m:bool) -> void:
 	var was_muted := muted
 	
 	muted = m
@@ -50,10 +74,25 @@ func set_muted(m:bool, force_notification:bool=false) -> void:
 	else:
 		_update_master_bus_volume()
 	
-	if was_muted != muted or force_notification:
+	if was_muted != muted:
 		emit_signal("muted_changed", muted)
 
 
 func _update_master_bus_volume() -> void:
 	var volume : float = master_volume * (MAX_VOLUME - MIN_VOLUME) + MIN_VOLUME
 	AudioServer.set_bus_volume_db(0, volume)
+
+
+func _save_to_file() -> void:
+	var file := File.new()
+	file.open(SETTINGS_FILE_PATH, File.WRITE)
+	
+	var dict_to_save := {
+		"master_volume": master_volume,
+	}
+	
+	var str_dict := JSON.print(dict_to_save)
+	file.store_string(str_dict)
+	
+	file.close()
+	
