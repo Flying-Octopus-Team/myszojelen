a8a61c7f762d6237df74a928ea2ef7da65230b37
Ktwsz <doroszuk20@gmail.com>
2021-05-23 16:44:51 +0200

    Added placeholder labels

--- project/src/MainMenu/InputButton.gd
+++ project/src/MainMenu/InputButton.gd
@@ -13,9 +13,16 @@ func _ready() -> void:
 		else:
 			text = event.as_text()
 
+
 func _pressed() -> void:
+	for action_hbox in $"../../".get_children():
+		for button in action_hbox.get_children():
+			if button is Button:
+				button.disable()
+	text = ">"+text+"<"
 	set_process_input(true)
 	pressed_button = true
+
 
 func _input(event):
 	if event is InputEventKey and pressed_button:
@@ -32,14 +39,16 @@ func _input(event):
 				InputMap.action_erase_event(action_name, action_list[0])
 			InputMap.action_add_event(action_name, event)
 			
-			for other_action in get_parent().get_children():
-				if other_action == self or other_action.text != scancode:
-					continue
-				
-				other_action.text = ""
-				InputMap.action_erase_event(other_action.action_name, InputMap.get_action_list(other_action.action_name)[0])
+			for other_action_hbox in $"../../".get_children():
+				for other_action in other_action_hbox.get_children():
+					if not other_action is Button or other_action == self or other_action.text != scancode:
+						continue
+
+					other_action.text = ""
+					InputMap.action_erase_event(other_action.action_name, InputMap.get_action_list(other_action.action_name)[0])
 
 			SteeringSave.save_input()
+		pressed_button = false
 	elif event is InputEventJoypadButton and pressed_button:
 		get_tree().set_input_as_handled()
 		set_process_input(false)
@@ -54,6 +63,11 @@ func _input(event):
 			InputMap.action_add_event(action_name, event)
 
 			SteeringSave.save_input()
+		pressed_button = false
 
 		
-		
+func disable() -> void:
+	if pressed_button == true:
+		set_process_input(false)
+		pressed_button = false
+		text = text.substr(1, text.length()-2)
