4a5a87b6dd14d3cc4bf4172480211a7510d758a6
Ktwsz <doroszuk20@gmail.com>
2021-05-26 23:57:43 +0200

    Added smaller methods to SteeringSave

--- project/src/PreloadedScripts/SteeringSave.gd
+++ project/src/PreloadedScripts/SteeringSave.gd
@@ -15,36 +15,49 @@ func _load_input() -> void:
 
 	var err = config_file.load(CONFIG_FILE)
 	if err == OK:
-		for action in config_file.get_section_keys("steering"):
-			InputMap.action_erase_event(action, InputMap.get_action_list(action)[0])
-			if config_file.get_value("steering", action) == "":
-				continue
-
-			var scancode = OS.find_scancode_from_string(config_file.get_value("steering", action))
-
-			var event = InputEventKey.new()
-			event.scancode = scancode
-
-			InputMap.action_add_event(action, event)
-
-		steering_type = config_file.get_value("steering_type", "name")
+		_load_from_file()
 	else:
 		save_input()
 
 
+func _load_from_file() -> void:
+	for action in config_file.get_section_keys("steering"):
+		InputMap.action_erase_event(action, InputMap.get_action_list(action)[0])
+
+		_set_action_to_keybind(action)
+
+	steering_type = config_file.get_value("steering_type", "name")
+
+
+func _set_action_to_keybind(action) -> void:
+	if config_file.get_value("steering", action) == "":
+		return
+
+	var scancode = OS.find_scancode_from_string(config_file.get_value("steering", action))
+
+	var event = InputEventKey.new()
+	event.scancode = scancode
+
+	InputMap.action_add_event(action, event)
+
+
 func save_input() -> void:
 	for action in INPUT_ACTIONS:
-		var action_list = InputMap.get_action_list(action)
-		if not action_list.empty():
-			var action_event = action_list[0]
-
-			var scancode = OS.get_scancode_string(action_event.scancode)
-			config_file.set_value("steering", action, scancode)
-		else:
-			config_file.set_value("steering", action, "")
+		_save_action_to_file(action)
 	
 	config_file.set_value("steering_type", "name", steering_type)
 	config_file.save(CONFIG_FILE)
+
+
+func _save_action_to_file(action: String) -> void:
+	var action_list = InputMap.get_action_list(action)
+	if not action_list.empty():
+		var action_event = action_list[0]
+
+		var scancode = OS.get_scancode_string(action_event.scancode)
+		config_file.set_value("steering", action, scancode)
+	else:
+		config_file.set_value("steering", action, "")
 
 
 func set_steering_type(value: String) -> void:
