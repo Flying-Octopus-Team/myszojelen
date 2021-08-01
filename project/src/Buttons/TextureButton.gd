4f9a181b8a39d893db525d7a2ee7b292a58ccd48
Ktwsz <doroszuk20@gmail.com>
2021-08-01 21:00:59 +0200

    Refactored texture button

--- project/src/Buttons/TextureButton.gd
+++ project/src/Buttons/TextureButton.gd
@@ -2,29 +2,18 @@ extends TextureButton
 
 export var upscale_on_hover := false
 export var downscale_on_press := false
-export var should_scale : bool = true
 
 
 func _ready() -> void:
 	rect_pivot_offset = rect_size * 0.5
 
-	if should_scale:
-		connect("mouse_entered", self, "_on_mouse_entered")
-		connect("mouse_exited", self, "_on_mouse_exited")
-
 	connect("button_down", self, "_on_button_down")
 	connect("button_up", self, "_on_button_up")
-
-	Settings.connect("audio_effects_volume_changed", self, "set_audio_volume")
-	set_audio_volume(Settings.audio_effects_volume)
 
 
 func _on_mouse_entered() -> void:
 	if upscale_on_hover:
 		_upscale()
-	
-	if not disabled:
-		$HoverSound.play()
 
 
 func _on_mouse_exited() -> void:
@@ -35,9 +24,6 @@ func _on_mouse_exited() -> void:
 func _on_button_down() -> void:
 	if downscale_on_press:
 		_downscale()
-	
-	if not disabled:
-		$ClickSound.play()
 
 
 func _on_button_up() -> void:
@@ -73,7 +59,3 @@ func handle_action(action: int) -> void:
 		return
 	
 	emit_signal("pressed")
-
-func set_audio_volume(value: float) -> void:
-	$HoverSound.set_volume_db(linear2db(value))
-	$ClickSound.set_volume_db(linear2db(value))
