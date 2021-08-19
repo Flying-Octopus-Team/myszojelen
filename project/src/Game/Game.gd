eca97b88bcb9f729d4da37c53f8b75931e0a8105
Ktwsz <doroszuk20@gmail.com>
2021-08-19 23:07:19 +0200

    fixed formatting

--- project/src/Game/Game.gd
+++ project/src/Game/Game.gd
@@ -73,8 +73,8 @@ func _on_tree_cut() -> void:
 	if not _is_game_running:
 		return
 	
-		trees_left -= 1
-		interface.set_trees_left(trees_left)
+	trees_left -= 1
+	interface.set_trees_left(trees_left)
 	
 	if trees_left <= 0:
 		_is_game_running = false
